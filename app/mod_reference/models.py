854de2a79418355e7d584950cc21179dd963dfb9
Tracy Liu <tracyliu981220@gmail.com>
2020-12-26 00:55:10 +0800

    fix: Cancel nullable setting of reference model

--- app/mod_reference/models.py
+++ app/mod_reference/models.py
@@ -5,8 +5,8 @@ from app.utils.db_base import Base
 class Reference(Base):
   meeting_id = db.Column(db.Integer, db.ForeignKey('meeting.id'))
 
-  title = db.Column(db.String(120), unique=True, nullable=False)
-  link = db.Column(db.String(300), unique=True, nullable=False)
+  title = db.Column(db.String(120), nullable=False)
+  link = db.Column(db.String(300), nullable=False)
 
   meeting = db.relationship('Meeting', foreign_keys=[meeting_id], back_populates='references')
 
